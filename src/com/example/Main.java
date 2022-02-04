e1daaf32c20e6e7128448b3ba9c1f8cfe9414c78
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-02-04 20:22:22 +0530

    Example
    Explicit dereference a variable with null

---
+++ src/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello from Java!");
+        String tempVar = "Won't be around for long!";
+        tempVar = null;
+        System.out.println(tempVar);
+    }
+}
