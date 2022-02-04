e485acd43de0e7fa0368bba9de3cddc330c7c8ed
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-02-04 23:17:34 +0530

    Math Class

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,10 +1,32 @@
 package com.example;
 public class Main {
-    public static void main(String args[]) {
+    public void mathClass() {
+        int intValue1 = 56;
+        int intValue2 = 42;
+        int result1 = intValue1 + intValue2;
+        System.out.println(result1);
+        int result2 = intValue1 - intValue2;
+        System.out.println(result2);
+        int result3 = intValue1 * intValue2;
+        System.out.println(result3);
+        int result4 = intValue1 / intValue2;
+        System.out.println(result4);
+        double result5 = (double) intValue1 / (double) intValue2;
+        System.out.println(result5);
+        long rounded = Math.round(result5);
+        System.out.println(rounded);
+        int neg = -5;
+        double absValue = Math.abs(neg);
+        System.out.println(absValue);
+    }
+    public static void main(String[] args) {
         // Write your code here
-        for (String s :
+        /* for (String s :
                 args) {
             System.out.println(s);
         }
+        */
+        Main m = new Main();
+        m.mathClass();
     }
 }
