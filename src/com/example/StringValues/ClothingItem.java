8a2cb68e12617ef1c2f51a39844f519c6172f9b4
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-02-19 21:49:15 +0530

    Example

---
+++ src/com/example/StringValues/ClothingItem.java
@@ -0,0 +1,15 @@
+package com.example.StringValues;
+
+public class ClothingItem {
+    public String type;
+
+    public static void main(String[] args) {
+        ClothingItem item = new ClothingItem();
+        item.type = "Hat";
+        item.displayItem();
+    }
+
+    private void displayItem() {
+        System.out.println("This is a " + this.type);
+    }
+}
