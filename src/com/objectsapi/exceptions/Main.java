8d08726061df1dbe2b4bd175b6c3b06ea9461e6b
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-05-22 08:44:09 +0530

    array index out of box ecxeption

--- src/com/objectsapi/exceptions/Main.java
+++ src/com/objectsapi/exceptions/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         String welcome = "Welcome";
         char[] chars = welcome.toCharArray();
-        char lastChar = chars[chars.length - 1];
+        char lastChar = chars[chars.length];
         System.out.println("last char:" + lastChar);
     }
 }
