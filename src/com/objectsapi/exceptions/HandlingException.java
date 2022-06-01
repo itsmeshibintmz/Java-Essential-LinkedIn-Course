0aee8ecf35aff2f16b240d182ff0f93fca1d9b59
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-06-01 23:13:26 +0530

    Before exception

--- src/com/objectsapi/exceptions/HandlingException.java
+++ src/com/objectsapi/exceptions/HandlingException.java
@@ -1,1 +1,10 @@
-public class HandlingException
+package com.objectsapi.exceptions;
+
+public class HandlingException {
+    public static void main(String[] args) {
+        String welcome = "Welcome";
+        char[] chars = welcome.toCharArray();
+        char lastChar = chars[chars.length - 1];
+        System.out.println("Last char:" + lastChar);
+    }
+}
