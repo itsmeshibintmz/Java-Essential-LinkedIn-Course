f7a601cb6ff31560f0987a8134940a5715f46626
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-06-01 22:57:53 +0530

    Create Result.java

---
+++ src/com/javaessential/java/Result.java
@@ -0,0 +1,15 @@
+package com.javaessential.java;
+
+public class Result {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello from Java!");
+        String s1 = "Hello from JShell";
+        System.out.println(s1);
+        int i1 = 5;
+        int i2 = 10;
+        int result = i1 + i2;
+        System.out.println( "The result is " + result);
+    }
+}
