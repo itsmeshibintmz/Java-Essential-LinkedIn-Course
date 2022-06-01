fbf63d34ce7f9007244d7ffe8dd77c18fc468f82
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-06-01 22:58:13 +0530

    Create Exception.java

---
+++ src/com/objectsapi/exceptions/Exception.java
@@ -0,0 +1,18 @@
+package com.objectsapi.exceptions;
+
+public class Exception {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//        int x = 1;
+//        String x = "Hello";
+
+//        String welcome = "Welcome";
+//        char[] chars = welcome.toCharArray();
+//        char lastChar = chars[chars.length];
+//        System.out.println("last char:" + lastChar);
+
+        String nothing = null;
+        System.out.println(nothing);
+        System.out.println("string length= " + nothing.length());
+    }
+}
