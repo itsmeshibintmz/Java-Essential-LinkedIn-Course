e712cb5e640dd1e522464528f6eec8b017eb2d45
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-06-01 23:38:01 +0530

    checked exception

--- src/com/objectsapi/exceptions/MultipleCatch.java
+++ src/com/objectsapi/exceptions/MultipleCatch.java
@@ -8,18 +8,25 @@ public class MultipleCatch {
             char lastChar = chars[chars.length - 1];
             System.out.println("Last char:" + lastChar);
 
-            String nothing = null;
-            System.out.println(nothing.length()); 
+//          String nothing = null;
+//          System.out.println(nothing.length()); 
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Array Index Out of exceptions");
             e.printStackTrace();
             return;
-        } catch (NullPointerException e) {
+/*         } catch (NullPointerException e) {
             System.out.println("NullPointerException");
             e.printStackTrace();
             return;
+*/
         } catch (Exception e) {
             System.out.println("Any exception");
+        }
+
+        try{
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         System.out.println("Code ran success");
     }
