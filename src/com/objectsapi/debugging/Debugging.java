c59d9ce3e66be4573bffa65d6018371682f90a58
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-06-01 22:58:22 +0530

    Debugging method

---
+++ src/com/objectsapi/debugging/Debugging.java
@@ -0,0 +1,12 @@
+package com.objectsapi.debugging;
+
+public class Debugging {
+    public static void main(String[] args) {
+        String welcome = "Welcome";
+        char[] chars = welcome.toCharArray();
+        for (char character :
+            chars) {
+                System.out.println(character);
+            }
+    }
+}
